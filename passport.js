ce5ec48eb7734721b693e5b9d44a2c91ee279084
Solomon Gifford <sgifford@blackmesh.com>
2014-12-16 13:20:03 -0500

    load the groups in passport

--- passport.js
+++ passport.js
@@ -124,12 +124,17 @@ exports = module.exports = function(app, passport) {
 
   passport.deserializeUser(function(id, done) {
     app.db.models.User.findOne({ _id: id }).populate('roles.admin').populate('roles.account').exec(function(err, user) {
-      if (user && user.roles && user.roles.admin) {
-        user.roles.admin.populate("groups", function(err, admin) {
-          done(err, user);
-        });
-      }
-      else {
+      if (user && user.roles) {
+        if (user.roles.admin) {
+            user.roles.admin.populate("groups", function (err, admin) {
+                done(err, user);
+            });
+        } else {
+            user.roles.account.populate("groups", function (err, admin) {
+                done(err, user);
+            });
+        }
+      } else {
         done(err, user);
       }
     });
